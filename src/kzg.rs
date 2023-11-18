898941ad1f349b615031a9d8c89fa6b3cfe6c832
eerkaijun <kaijuneer@gmail.com>
2023-11-18 11:55:52 +0300

    setup and commit function

--- src/kzg.rs
+++ src/kzg.rs
@@ -1,3 +1,5 @@
+use std::ops::Mul;
+
 use ark_ff::Field;
 use ark_ec::pairing::Pairing;
 
@@ -5,6 +7,7 @@ pub struct KZG<E: Pairing> {
     pub g1: E::G1,
     pub g2: E::G2,
     pub degree: usize,
+    pub crs: Vec<E::G1>,
 }
 
 impl <E:Pairing> KZG<E> {
@@ -13,15 +16,22 @@ impl <E:Pairing> KZG<E> {
             g1,
             g2,
             degree,
+            crs: vec![],
         }
     }
 
     pub fn setup(&mut self, secret: E::ScalarField) {
-
+        for i in 0..self.degree+1 {
+            self.crs.push(self.g1.mul(secret.pow(&[i as u64])));
+        }
     }
 
-    pub fn commit(&self, poly: &[E::ScalarField]) {
-
+    pub fn commit(&self, poly: &[E::ScalarField]) -> E::G1 {
+        let mut commitment = self.g1.mul(E::ScalarField::ZERO);
+        for i in 0..self.degree+1 {
+            commitment += self.crs[i] * poly[i];
+        }
+        commitment
     }
 
     pub fn open(&self, poly: &[E::ScalarField], point: E::ScalarField) -> E::ScalarField {
