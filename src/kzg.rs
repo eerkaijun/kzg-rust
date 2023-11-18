12887c4b1eb9fdb6b75bd411196bd9165c5cde1e
eerkaijun <kaijuneer@gmail.com>
2023-11-18 16:46:54 +0300

    test works

--- src/kzg.rs
+++ src/kzg.rs
@@ -11,38 +11,6 @@ pub struct KZG<E: Pairing> {
 }
 
 impl <E:Pairing> KZG<E> {
-
-    // helper function for polynomial division
-    pub fn div(p1: &[E::ScalarField], p2: &[E::ScalarField]) -> Result<Vec<E::ScalarField>, &'static str> {
-        if p2.is_empty() || p2.iter().all(|&x| x == E::ScalarField::ZERO) {
-            return Err("Cannot divide by zero polynomial");
-        }
-    
-        if p1.len() < p2.len() {
-            return Ok(vec![E::ScalarField::ZERO]);
-        }
-    
-        let mut quotient = vec![E::ScalarField::ZERO; p1.len() - p2.len() + 1];
-        let mut remainder: Vec<E::ScalarField> = p1.to_vec();
-    
-        while remainder.len() >= p2.len() {
-            let coeff = *remainder.last().unwrap() / *p2.last().unwrap();
-            let pos = remainder.len() - p2.len();
-    
-            quotient[pos] = coeff;
-    
-            for (i, &factor) in p2.iter().enumerate() {
-                remainder[pos + i] -= factor * coeff;
-            }
-    
-            while let Some(true) = remainder.last().map(|x| *x == E::ScalarField::ZERO) {
-                remainder.pop();
-            }
-        }
-    
-        Ok(quotient)
-    }
-
     pub fn new(g1: E::G1, g2: E::G2, degree: usize) -> Self {
         Self {
             g1,
@@ -70,10 +38,7 @@ impl <E:Pairing> KZG<E> {
 
     pub fn open(&self, poly: &[E::ScalarField], point: E::ScalarField) -> E::G1 {
         // evaluate the polynomial at point
-        let mut value = E::ScalarField::ZERO;
-        for i in 0..self.degree+1 {
-            value += poly[i] * point.pow(&[i as u64]);
-        }
+        let value = self.evaluate(poly, point);
 
         // initialize denominator
         let denominator = [-point, E::ScalarField::ONE];
@@ -107,5 +72,45 @@ impl <E:Pairing> KZG<E> {
         let lhs = E::pairing(pi, self.g2_tau - self.g2.mul(point));
         let rhs = E::pairing(commitment - self.g1.mul(value), self.g2);
         lhs == rhs
+    }
+
+    // helper function to evaluate polynomial at a point
+    pub fn evaluate(&self, poly: &[E::ScalarField], point: E::ScalarField) -> E::ScalarField {
+        let mut value = E::ScalarField::ZERO;
+        for i in 0..poly.len() {
+            value += poly[i] * point.pow(&[i as u64]);
+        }
+        value
+    }
+
+    // helper function for polynomial division
+    pub fn div(p1: &[E::ScalarField], p2: &[E::ScalarField]) -> Result<Vec<E::ScalarField>, &'static str> {
+        if p2.is_empty() || p2.iter().all(|&x| x == E::ScalarField::ZERO) {
+            return Err("Cannot divide by zero polynomial");
+        }
+    
+        if p1.len() < p2.len() {
+            return Ok(vec![E::ScalarField::ZERO]);
+        }
+    
+        let mut quotient = vec![E::ScalarField::ZERO; p1.len() - p2.len() + 1];
+        let mut remainder: Vec<E::ScalarField> = p1.to_vec();
+    
+        while remainder.len() >= p2.len() {
+            let coeff = *remainder.last().unwrap() / *p2.last().unwrap();
+            let pos = remainder.len() - p2.len();
+    
+            quotient[pos] = coeff;
+    
+            for (i, &factor) in p2.iter().enumerate() {
+                remainder[pos + i] -= factor * coeff;
+            }
+    
+            while let Some(true) = remainder.last().map(|x| *x == E::ScalarField::ZERO) {
+                remainder.pop();
+            }
+        }
+    
+        Ok(quotient)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 pub mod kzg;
+use kzg::KZG;
+use ark_std::UniformRand;
+use ark_bls12_381::{Bls12_381, Fr, G1Projective as G1, G2Projective as G2};
 
 fn main() {
-    println!("Hello, world!");
+    // initialize kzg instance
+    let mut rng = ark_std::test_rng();
+    let degree = 16;
+    let mut kzg_instance = KZG::<Bls12_381>::new(
+        G1::rand(&mut rng),
+        G2::rand(&mut rng),
+        degree
+    );
+
+    // trusted setup ceremony
+    let secret = Fr::rand(&mut rng);
+    kzg_instance.setup(secret);
+
+    // generate a random polynomial and commit it
+    let poly = vec![Fr::rand(&mut rng); degree+1];
+    let commitment = kzg_instance.commit(&poly);
+
+    // generate a random point and open the polynomial at that point
+    let point = Fr::rand(&mut rng);
+    let pi = kzg_instance.open(&poly, point);
+
+    // verify the proof
+    let value = kzg_instance.evaluate(&poly, point);
+    assert!(kzg_instance.verify(point, value, commitment, pi));
 }
