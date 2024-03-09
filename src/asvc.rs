70f40ce2e278b73ffa9c841e65790619e0eb132d
eerkaijun <kaijuneer@gmail.com>
2024-03-09 16:48:10 +0800

    set up vector commitment

--- src/asvc.rs
+++ src/asvc.rs
@@ -1,18 +1,40 @@
+use std::ops::Mul;
+use ark_ff::Field;
 use ark_ec::pairing::Pairing;
 
 pub struct ASVC<E: Pairing> {
-    pub proving_key: E::G1,
-    pub verification_key: E::G1,
+    pub g1: E::G1,
+    pub g2: E::G2,
+    pub g2_tau: E::G2,
+    pub degree: usize,
+    pub crs_g1: Vec<E::G1>,
+    pub crs_g2: Vec<E::G2>
 }
 
 impl <E: Pairing> ASVC<E> {
-
-    // TODO: key generation
-    pub fn new(g1: E::G1) -> Self {
+    pub fn new(g1: E::G1, g2: E::G2, degree: usize) -> Self {
         Self {
-            proving_key: g1,
-            verification_key: g1,
+            g1,
+            g2,
+            g2_tau: g2.mul(E::ScalarField::ZERO),
+            degree,
+            crs_g1: vec![],
+            crs_g2: vec![],
         }
+    }
+
+    // the `secret` should be generated through secure MPC
+    pub fn setup(&mut self, secret: E::ScalarField) {
+        for i in 0..self.degree+1 {
+            self.crs_g1.push(self.g1.mul(secret.pow(&[i as u64])));
+            self.crs_g2.push(self.g2.mul(secret.pow(&[i as u64])));
+        }
+        self.g2_tau = self.g2.mul(secret);
+    }
+
+    // commit the lagrange basis of the vector
+    pub fn vector_commit(&self, vector: &[E::ScalarField]) {
+        
     }
 
     // aggregate multiple proofs into one subvector commitment
